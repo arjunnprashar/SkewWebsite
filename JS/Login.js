eadba3441d65616d91c77563a54e3b43f4e998ba
Arjun Prashar <arjun0109.be20@chitkara.edu.in>
2023-01-22 18:46:19 +0530

    Skew Commit

---
+++ JS/Login.js
@@ -0,0 +1,16 @@
+const options = {
+    bottom: '64px',
+    right: '32px',
+    left: 'unset',
+    time: '0.5s',
+    mixColor: '#fff',
+    backgroundColor: '#fff',
+    buttonColorDark: '#100f2c',
+    buttonColorLight: '#fff',
+    saveInCookies: true,
+    label: '🌓',
+    }
+
+    const darkmode = new Darkmode(options);
+
+    darkmode.showWidget();
